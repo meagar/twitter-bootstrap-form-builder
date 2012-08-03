b7a86875c244e2d6b8788aa81a0b79715e1e6628
Matthew Eagar <matthew.eagar@mosaic.com>
2012-08-03 00:03:32 -0400

    Add real checkboxes

--- lib/twitter-bootstrap-form-builder/form_builder.rb
+++ lib/twitter-bootstrap-form-builder/form_builder.rb
@@ -14,33 +14,32 @@ module MNE
         opts[:class] << "control-group"
         opts[:class] << field if field
 
-        opts[:class] << "error" if @object.errors.messages.has_key? field
+        opts[:class] << "error" if @object.errors.messages.has_key?(field) && @object.errors.messages[field].any?
 
         @template.content_tag(:div, opts) do
           @template.capture &block
         end
       end
 
-      %w(text_field phone_field password_field email_field number_field file_field text_area select check_box).each do |method_name|
+      %w(text_field phone_field password_field email_field number_field file_field text_area select).each do |method_name|
         define_method method_name.to_sym do |field, *args|
 
-          # extract the options for the label tag
-          opts = args.find { |a| a.is_a?(Hash) && a.has_key?(:label) }
-          label_opts = opts ? opts[:label] : []
+          # find the options hash, and extract the options for the label tag
+          opts       = extract_options(args)
+          label_opts = extract_sub_options(opts, :label)
 
           # If label is false, we're rendering the field without modification
           return super(field, *args) if label_opts === false
 
+          # Add the TB class to the label
+          label_opts << { :class => "control-label" }
+
           # create a help-block if present
-          help_opts = args.find { |a| a.is_a?(Hash) && a.has_key?(:help_block) }
-          help_block = help_opts && help_opts[:help_block] ? @template.content_tag(:p, help_opts[:help_block], :class => "help-block") : ""
+          help_block = opts[:help_block] ? @template.content_tag(:p, opts[:help_block], :class => "help-block") : ""
 
           # propogate properties of control group up
-          h = args.find { |a| a.is_a?(Hash) && a.has_key?(:control_group) }
-          control_group_opts = (h && h[:control_group]) || {}
-          control_group_opts[:class] =  control_group_opts[:class] ?  control_group_opts[:class] + " #{method_name}" : "#{method_name}"
-
-          label_opts = Array[label_opts] << { :class => "control-label" }
+          control_group_opts = opts[:control_group] || {}
+          control_group_opts[:class] = control_group_opts[:class] ? control_group_opts[:class] + " #{method_name}" : "#{method_name}"
 
           control_group(field, control_group_opts) do
             label(field, *label_opts) + @template.content_tag(:div, :class => "controls") do
@@ -48,12 +47,55 @@ module MNE
             end
           end.html_safe
         end
+      end
 
+
+      # Special handling for check boxes, which can have two labels
+      def check_box(field, options = {}, checked_value = "1", unchecked_value = "0")
+        label_opts = extract_sub_options(options, :label)
+        about_opts = extract_sub_options(options, :about)
+
+        return super if label_opts == false
+
+        #raise [object.errors.messages, errors_for(field)].inspect
+        # Add the TB class to the thelabel
+        label_opts << { :class => "control-label" } unless label_opts.nil?
+        about_opts << { :class => "checkbox" } if about_opts.any?
+
+        control_group_opts = options[:control_group] || {}
+
+        label_tag = (label_opts ? label(field, *label_opts) : "").html_safe
+        check_box = super(field, options, checked_value, unchecked_value).html_safe
+
+        control_group(field, control_group_opts) do
+          label_tag + @template.content_tag(:div, :class => "controls") do
+            check_box = label(field, *about_opts) { check_box + about_opts[0] } if about_opts
+            check_box + errors_for(field)
+          end
+        end.html_safe
+      end
+
+      protected
+
+      def extract_options(args)
+        args.find { |a| a.is_a?(Hash) && a.has_key?(:label) } || {}
+      end
+
+      def extract_sub_options(opts, key = :label)
+        sub_opts = opts.keys.include?(key) ? opts[key] : []
+        return false if sub_opts === false
+        return nil   if sub_opts.nil?
+        Array(sub_opts)
+      end
+
+      def label_opts(opts, key = :label)
+        label_opts = extract_sub_options(opts, key)
+        label_opts << { :class => "control-label" }
       end
 
       def errors_for(field)
         @template.content_tag(:span, "#{object.class.human_attribute_name(field)} #{object.errors.messages[field].to_sentence}",
-                              :class => "help-inline") if object.errors.messages[field].any?
+                              :class => "help-inline") if object.errors.messages.has_key?(field) && object.errors.messages[field].any?
       end
 
     end
